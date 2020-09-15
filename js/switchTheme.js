7f8ca19ddb848902f090ef98036965e6eaf182bb
milkndcoffee <milkndcoffee@gmail.com>
2020-09-15 17:24:58 -0500

    finished animation, added time, removed light css for tools

--- js/switchTheme.js
+++ js/switchTheme.js
@@ -1,46 +1,69 @@
 function toggleTheme() {
-  let containerDom = document.getElementById('container');
+  let bodyDom = document.getElementById('body');
   let themeButton = document.getElementById('theme-button');
   let buttonAnim = document.getElementById('button-anim');
 
-  if (containerDom.classList.contains('light-theme')) {
-    /* To Dark Theme */
-    //animation
+  bodyDom.classList.contains('light-theme') 
+  ? animateItems(bodyDom, themeButton, buttonAnim, 'toDark') 
+  : animateItems(bodyDom, themeButton, buttonAnim, 'toLight');
+}
+
+//called onload in body of html
+function setTheme(){
+  let bodyDom = document.getElementById('body');
+  let themeButton = document.getElementById('theme-button');
+
+  let dateObj = new Date();
+  let hour = dateObj.getHours();
+  //light theme 7AM - 5:59 PM ... dark theme 6:PM - 6:59AM
+  hour >= 18 || hour < 7 ? bodyDom.classList.add('dark-theme') : bodyDom.classList.add('light-theme')
+  bodyDom.classList.contains('light-theme') ? themeButton.innerHTML = 'Dark' : themeButton.innerHTML = 'Light';
+}
+
+function updateTime(){
+  let timeDom = document.getElementById('time');
+  let dateObj = new Date();
+  let hour = dateObj.getHours();
+  let minute = dateObj.getMinutes();
+  let meridiem = hour >= 12 && !(hour == 24) ? 'PM' : 'AM';
+  hour = hour > 12 ? hour - 12 : hour;
+  minute = minute < 10 ? '0' + minute: minute; //adds '0' when there's only a single integer
+  let time = (hour + ':' + minute + ' ' + meridiem);
+  timeDom.innerHTML = time;
+  setTimeout(updateTime, 1000);
+}
+
+function animateItems(bodyDom, themeButton, buttonAnim, animation){
+  if (animation == 'toDark'){
     buttonAnim.classList.add('anim-enlarge');
     buttonAnim.classList.add('anim-dark-to-light');
-    buttonAnim.classList.add('anim-fade-in');
     themeButton.classList.add('anim-dark-to-light');
-
     if (buttonAnim.classList.contains('anim-fade-in')) { buttonAnim.classList.remove('anim-fade-in'); }
+
     setTimeout(function () {
       buttonAnim.classList.remove('anim-enlarge');
       buttonAnim.classList.remove('anim-dark-to-light');
       themeButton.classList.remove('anim-dark-to-light');
       buttonAnim.classList.remove('anim-fade-in');
 
-
       buttonAnim.classList.add('anim-light-to-dark');
       themeButton.classList.add('anim-light-to-dark');
       buttonAnim.classList.add('anim-fade-in');
+      themeButton.classList.add('anim-fade-in');
 
       //recolor
       themeButton.innerHTML = "Light";
-      containerDom.classList.remove('light-theme');
-      containerDom.classList.add('dark-theme');
+      bodyDom.classList.remove('light-theme');
+      bodyDom.classList.add('dark-theme');
     }, 1000);
-
-
-
   } else {
     /* To Light Theme */
-    //animation
     buttonAnim.classList.add('anim-enlarge');
     buttonAnim.classList.add('anim-light-to-dark');
     themeButton.classList.add('anim-light-to-dark');
-    buttonAnim.classList.add('anim-fade-in');
-
 
     buttonAnim.classList.remove('anim-fade-in');
+    themeButton.classList.remove('anim-fade-in');
 
     setTimeout(function () {
       buttonAnim.classList.remove('anim-enlarge');
@@ -51,13 +74,12 @@ function toggleTheme() {
       buttonAnim.classList.add('anim-dark-to-light');
       themeButton.classList.add('anim-dark-to-light');
       buttonAnim.classList.add('anim-fade-in');
+      themeButton.classList.add('anim-fade-in');
 
       //recolor
       themeButton.innerHTML = "Dark";
-      containerDom.classList.remove('dark-theme');
-      containerDom.classList.add('light-theme');
+      bodyDom.classList.remove('dark-theme');
+      bodyDom.classList.add('light-theme');
     }, 1000);
-
-
   }
 }
